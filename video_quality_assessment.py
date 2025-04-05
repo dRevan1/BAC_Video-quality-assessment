635330195e3b608239b0691252e4802d0dc0ea0f
dRevan1 <filippokorny699@gmail.com>
2025-04-05 20:15:39 +0200

    Pridaná druhá časť (verzia) UI a načítavaie/ukladanie s CSV. Fungujú obe časti ako karty v jednom okne. Pridané CSV súbory pre testovanie. Už chýbajú iba používateľské prvky pre chybné vstupné hodnoty a doplnenie práce so súbormi v ďalších formátoch (JSON, excel).

--- video_quality_assessment.py
+++ video_quality_assessment.py
@@ -1,4 +1,6 @@
 import csv
+import json
+from openpyxl import Workbook
 import tensorflow as tf
 import keras as keras
 from keras import Model, layers, callbacks, Input
@@ -10,6 +12,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plotter
 from PySide6 import QtWidgets
 from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import Qt
 
 num.set_printoptions(suppress=True)
 scene_list = []
@@ -76,13 +79,15 @@ def train_network_configuration_test(neurons_list, activation_function, x_train,
 
     layer_list.append(layers.Dense(neurons_list[0], activation=activation_function)(input_layer))
     for i in range(1, len(neurons_list)):
-        layer_list.append(layers.Dense(neurons_list[i], activation=activation_function, kernel_regularizer=keras.regularizers.l2(0.04))(layer_list[-1]))
+        layer_list.append(layers.Dense(neurons_list[i], activation=activation_function)(layer_list[-1]))
+        layer_list.append(layers.Dropout(0.3)(layer_list[-1]))
     output_layer = layers.Dense(1, activation='linear')(layer_list[-1])
 
     model = Model(inputs=input_layer, outputs=output_layer)
     model.compile(optimizer='adam', loss='mse', metrics=['mse'])
-    early_stop = callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=1, restore_best_weights=True)
-    model.fit(x_train, y_train, epochs=100, batch_size=32, validation_data=(x_test, y_test), callbacks=[early_stop])
+    #early_stop = callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=1, restore_best_weights=True)
+    #╠reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, min_lr=1e-6)
+    model.fit(x_train, y_train, epochs=1, batch_size=32, validation_data=(x_test, y_test))
     
     test_loss = model.evaluate(x_test, y_test)
     training_results.append([neurons_list, test_loss])
@@ -294,7 +299,7 @@ pca = PCA(n_components=4)
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 
-model = train_network_configuration_test([256, 128, 64], 'relu', X_train, Y_train, X_test, Y_test, [])
+model = train_network_configuration_test([150, 128, 64, 32], 'relu', X_train, Y_train, X_test, Y_test, [])
 
 def submit_for_prediction():
     scene = window.scene_combo.currentIndex()
@@ -305,13 +310,8 @@ def submit_for_prediction():
     bitrate = float(window.bitrate_input.text())
     ssim = float(window.ssim_input.text())
     vmaf = float(window.vmaf_input.text())
-
-    input_data = num.array([[scene, resolution, bitrate, codec, packet_loss, ssim, vmaf]])
-    input_data = Scaler.transform(input_data)
-    input_data = pca.transform(input_data)
-
-    prediction = model.predict(input_data)
-    window.result_label.setText(str(prediction[0][0]))
+    prediction = predict_video_quality(scene, resolution, bitrate, codec, packet_loss, ssim, vmaf)
+    window.result_label.setText(str(prediction))
 
 def refresh_ui():
     window.result_label.setText("")
@@ -320,16 +320,94 @@ def refresh_ui():
     window.vmaf_input.clear()
     window.loss_input.clear()
 
+def save_file_dialog():
+    file_dialog = QtWidgets.QFileDialog()
+    file_dialog.setAcceptMode(QtWidgets.QFileDialog.AcceptSave)
+    file_dialog.setNameFilters(["CSV files (*.csv)", "Excel files (*.xlsx)", "JSON files (*.json)"])
+    file_dialog.setDefaultSuffix("csv")
+    file_dialog.setViewMode(QtWidgets.QFileDialog.List)
+    header = ["Scene", "Resolution", "Bitrate", "Codec", "Packet loss", "SSIM", "VMAF", "MOS"]
+
+    if file_dialog.exec():
+        selected_file = file_dialog.selectedFiles()[0]
+        with open(selected_file, mode="w", newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=";")
+            csv_writer.writerow(header)
+            for row in range(window.predictions_table.rowCount()):
+                row_data = []
+                for column in range(window.predictions_table.columnCount()):
+                    item = window.predictions_table.item(row, column)
+                    row_data.append(item.text() if item is not None else "")
+                csv_writer.writerow(row_data)
+
+def get_file_data():
+    file_dialog = QtWidgets.QFileDialog()
+    file_dialog.setFileMode(QtWidgets.QFileDialog.ExistingFiles)
+    file_dialog.setNameFilter("CSV files (*.csv)")
+    file_dialog.setViewMode(QtWidgets.QFileDialog.List)
+
+    if file_dialog.exec():
+        selected_file = file_dialog.selectedFiles()[0]
+        with open(selected_file, mode="r") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=";")
+            window.predictions_table.setRowCount(0)
+            window.predictions_table.setSortingEnabled(False)
+            window.predictions_table.horizontalHeader().setSortIndicator(-1, Qt.AscendingOrder)
+            data = [row for row in csv_reader if any(cell.strip() for cell in row)]
+        return data
+    else:
+        return None
+        
+def open_file_dialog():
+    table_data = get_file_data()
+    if table_data:
+        for row in table_data:
+            window.predictions_table.insertRow(window.predictions_table.rowCount())
+            for column, value in enumerate(row):
+                window.predictions_table.setItem(window.predictions_table.rowCount() - 1, column, QtWidgets.QTableWidgetItem(value))
+            codec = (0 if row[1] == "H.264" else 1)
+            predicted_value = predict_video_quality(scene_switch(row[0]), resolution_switch(row[2]), int(row[3]), codec, float(row[4]), float(row[5]), float(row[6]))
+            window.predictions_table.setItem(window.predictions_table.rowCount() - 1, 7, QtWidgets.QTableWidgetItem(str(predicted_value)))
+        window.predictions_table.setSortingEnabled(True)
+        
+def open_results_file_dialog():
+    table_data = get_file_data()
+    if table_data:
+        if table_data[0][:8] == ["Scene", "Resolution", "Bitrate", "Codec", "Packet loss", "SSIM", "VMAF", "MOS"]:
+            for i in range(1, len(table_data)):
+                window.predictions_table.insertRow(window.predictions_table.rowCount())
+                for column, value in enumerate(table_data[i]):
+                    window.predictions_table.setItem(window.predictions_table.rowCount() - 1, column, QtWidgets.QTableWidgetItem(value))
+            window.predictions_table.setSortingEnabled(True)              
+
+def predict_video_quality(scene, resolution, bitrate, codec, packet_loss, ssim, vmaf):
+    input_data = num.array([[scene, resolution, bitrate, codec, packet_loss, ssim, vmaf]])
+    input_data = Scaler.transform(input_data)
+    input_data = pca.transform(input_data)
+
+    prediction = model.predict(input_data, verbose=0)
+    return prediction[0][0]
+    
 loader = QUiLoader()
 app = QtWidgets.QApplication([])
-window = loader.load("VQA_ui.ui")
+window = loader.load("VQA_ui_new.ui")
 window.setWindowTitle("Video Quality Assessment")
+window.submit_button.setFocus()
 
 window.resolution_combo.addItems(["HD (720p)", "FHD (1080p)", "UHD (2160p)"])
 window.codec_combo.addItems(["H.264", "H.265"])
 window.scene_combo.addItems(["Campfire", "Construction", "Runners", "Rush", "Tall", "Wood"])
+window.predictions_table.setSortingEnabled(True)
+window.predictions_table.model().rowsInserted.connect(lambda: window.save_button.setEnabled(window.predictions_table.rowCount() > 0))
+window.predictions_table.model().rowsRemoved.connect(lambda: window.save_button.setEnabled(window.predictions_table.rowCount() > 0))
+
 window.submit_button.clicked.connect(submit_for_prediction)
 window.refresh_button.clicked.connect(refresh_ui)
+window.file_screen_button.clicked.connect(lambda: window.stackedWidget.setCurrentWidget(window.page_file))
+window.home_button.clicked.connect(lambda: window.stackedWidget.setCurrentWidget(window.page_main))
+window.load_file_button.clicked.connect(open_file_dialog)
+window.load_results_button.clicked.connect(open_results_file_dialog)
+window.save_button.clicked.connect(save_file_dialog)
 
 
 
